49b9e0eeff49712a13b2b85b1303861e627c4022
mitsuo <oono0114@gmail.com>
2018-10-23 10:46:39 +0900

    tmp

--- oncode.py
+++ oncode.py
@@ -1,49 +1,76 @@
 import os
 import pickle
+import uuid
 import time
+from concurrent.futures import ThreadPoolExecutor
 
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+executor = ThreadPoolExecutor(max_workers=10)
+worker_map = {}
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+@app.route('/fetch', methods=['GET'])
+def fetch():
+    pid = request.args.get('pid')
+    result = {}
+    result["tests"] = []
+    with open(pid + "_result.txt", "rb") as f:
+        result["result"] = pickle.load(f)
+    with open(pid + "_case.txt", "rb") as f:
+        result["case"] = pickle.load(f)
+    # result["expect"] = case[1]
+    # result["verdict"] = (result["result"] == case[1])
+    # result["spent_time"] = "%-0.5f sec" % (end - start)
+    # result["tests"].append(results)
+    # result["executed_code"] = code[:]
+
+
 
 @app.route('/execute', methods=['POST'])
 def execute():
-    question = {"call": "add(*sys.argv[1:])", "testcases": [("1 2", 3), ("3 4", 7), ("4 5", 9)]}
-    result = {}
+    question = {"call": "add(*sys.argv[2:])", "testcases": [("1 2", 3), ("3 4", 7), ("4 5", 9)]}
     code = "No Code"
     if request.method == 'POST':
         code = request.form['code']
         q = question["call"]
-        code += f"""
-import sys
-import pickle 
-with open("result.txt", "wb") as f:
-    f.write(pickle.dumps({q}))
-        """
-    with open("tmp.py", "w") as f:
-        f.write(code)
-    result["tests"] = []
-    # subprocess.call(["tmp.py", "1", "2"])
-    for case in question["testcases"]:
-        results = {"input": case[0]}
-        start = time.time()
-        os.system(f"tmp.py {case[0]}")
-        end = time.time()
-        with open("result.txt", "rb") as f:
-            results["result"] = pickle.load(f)
-        results["expect"] = case[1]
-        results["verdict"] = (results["result"] == case[1])
-        results["spent_time"] = "%-0.5f sec" % (end - start)
-        result["tests"].append(results)
-    result["executed_code"] = code[:]
+        runcode = f"""
+{code}
 
-    return render_template('execute.html', result=result)
+import sys
+import pickle
+import time 
+with open(sys.argv[1] + "_result.txt", "wb") as f:
+    start = time.time()
+    result = {{}}
+    result["output"] = {q}
+    end = time.time()
+    f.write(pickle.dumps(result))
+    
+    time.sleep(1)
+        """
+        with open("tmp.py", "w") as f:
+            f.write(runcode)
+
+    # subprocess.call(["tmp.py", "1", "2"])
+        for case in question["testcases"]:
+            pid = str(uuid.uuid4())
+            case = {"input": case[0], "expect" : case[1], "code" : runcode}
+            print(pid, case)
+            def ex():
+                print("excuting.." + f"tmp.py {pid} {case[0]}")
+                # os.system(f"tmp.py {pid} {case[0]}")
+            executor.submit(ex)
+
+            with open(pid+ "_case.txt", "wb") as f:
+                f.write(pickle.dumps(case))
+
+    return render_template('execute.html', result={})
 
 
 if __name__ == '__main__':
