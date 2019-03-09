9d57ea6ce08f2d7a875541967ae68e8e9d007ffb
takahiro.ono <oono0114@gmail.com>
2019-03-09 16:55:36 +0900

    change config

--- server/websocket_server.py
+++ server/websocket_server.py
@@ -8,13 +8,13 @@ handler.setFormatter(logging.Formatter("%(asctime)s %(name)-12s %(funcName)-7s %
 logger.addHandler(handler)
 logger.propagate = False
 
-log_level = logging.DEBUG
+log_level = logging.INFO
 handler.setLevel(log_level)
 logger.setLevel(log_level)
 
 if __name__ == "__main__":
     PORT = 9001
     logger.info("Initializing...")
-    server = OnCodeServer(PORT, log_level, logger)
+    server = OnCodeServer(PORT, logger)
     logger.info("server started")
     server.run_forever()
